8c4c6f32855edbcc388c4d9335020183dd69a34b
caamartinezbu <carlosmartinez622@gmail.com>
2023-06-14 00:23:35 -0500

    creando template con blade

---
+++ resources/views/template.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pry web</title>
+</head>
+
+<body>
+    <p>
+        <a href="{{ route('home')}}">Home</a>
+        <a href="{{ route('blog')}}">Blog</a>
+    </p>
+
+    <hr>
+
+
+    @yield ('content')
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,26 +20,28 @@ use Illuminate\Support\Facades\Route;
  * Route::put   | Actualizar
  * 
  * 
-*/
+ */
 
-Route::get('/', function () { /**si vamos a la raiz obtendremos una vista llamada welcome */
+Route::get('/', function () {
+    /**si vamos a la raiz obtendremos una vista llamada welcome */
     return view('home');
-});
+})->name('home');
 
-Route::get('blog', function () { 
-      //consulta a base de datos
-
-    $posts =[
-        ['id' =>1, 'title' => 'PHP', 'slug' => 'php'],
-        ['id' =>2, 'title' => 'LARAVEL', 'slug' => 'laravel'],
-    ];
-
-    return view('blog',['posts'=>$posts]);
-});
-
-Route::get('blog/{slug}', function ($slug) { /***/
+Route::get('blog', function () {
     //consulta a base de datos
 
-    $post= $slug;
-    return view('post',['post'=>$post]);
-});
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'LARAVEL', 'slug' => 'laravel'],
+    ];
+
+    return view('blog', ['posts' => $posts]);
+})->name('blog');
+
+Route::get('blog/{slug}', function ($slug) {
+    /***/
+    //consulta a base de datos
+
+    $post = $slug;
+    return view('post', ['post' => $post]);
+})->name('post');
