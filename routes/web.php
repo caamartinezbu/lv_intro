37d46f3e8be0b2a1a71b2948e3b2b64b7a241d98
caamartinezbu <carlosmartinez622@gmail.com>
2023-06-14 15:19:07 -0500

    mejoramiento del enrutador creando una agrupacion para los metodos

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,8 +13,20 @@ use Illuminate\Support\Facades\Route;
  * 
  */
 
-Route::get('/',  [PageController::class,'home'])->name('home');
 
-Route::get('blog', [PageController::class,'blog'])->name('blog');
 
-Route::get('blog/{slug}',  [PageController::class,'post'])->name('post');
+/** 
+ * aqui llamamos en a cada metodo  que se creo en el controlador PageController y se le asigna a cada ruta correspondiente
+ Route::get('/',  [PageController::class, 'home'])->name('home');
+
+ Route::get('blog', [PageController::class, 'blog'])->name('blog');
+
+ Route::get('blog/{slug}',  [PageController::class, 'post'])->name('post');
+*/
+// se crea una agupacion con el controlador general PageController para asi solo llamar el metodo que se necesita.
+Route::controller(PageController::class)->group(function(){
+
+    Route::get('/',              'home')->name('home');
+    Route::get('blog',           'blog')->name('blog');
+    Route::get('blog/{slug}',    'post')->name('post');
+});
