15d0e1fcb04031e7be47396dfca6cd0f09d3383a
caamartinezbu <carlosmartinez622@gmail.com>
2023-07-14 22:58:33 -0500

    add vista post admin creando metodo controller y view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;     
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(){
-        return view('post.index');
+        return view('post.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
     }
 }
