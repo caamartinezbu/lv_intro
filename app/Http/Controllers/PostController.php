4db3985578049e188b9b480ac1fc0ca2302810cf
caamartinezbu <carlosmartinez622@gmail.com>
2023-07-17 16:40:59 -0500

    Add registrer duplicate
    se modifica las funciones store() y update() para evitar registro duplicados en el campo slug especificamente. ya que esta se creaba a partir del titulo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,35 +11,67 @@ use function Pest\Laravel\post;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index() //listar post
     {
-        return view('posts.index', ['posts' => Post::latest()->paginate() ]);
+        return view('posts.index', ['posts' => Post::latest()->paginate()]);
     }
 
-//creamos el método eliminar, para eliminar publicaciones y le decimos que nos etorne atras en las rutas usando el en rutamiento de laravel nativo
-    
+    //creamos el método eliminar, para eliminar publicaciones y le decimos que nos etorne atras en las rutas usando el en rutamiento de laravel nativo
 
-    public function create(Post $post){
-        return view('posts.create',['post'=>$post]);
+
+    public function create(Post $post)
+    { //formulario de crear
+        return view('posts.create', ['post' => $post]);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    { // función de crear y guardar
+        $request->validate([
+            'title'=> 'required',
+            'slug'=> 'required|unique:posts,slug', //unique:table,column,except,id',
+            'body'=> 'required',
 
+         ],[
+            'title.required'=>'Debe ingresar el título.',
+            'slug.required'=>'Debe ingresar una URL diferente',
+            'body.required'=>'Debe ingresar el contenido del post.',
+        ]);
         $post = $request->user()->posts()->create([
-            'title' => $title = $request->title,
-            'slug' => Str::slug($title),
+            'title' => $request->title,
+            'slug' => $request->slug,
             'body' => $request->body,
 
         ]);
-        return redirect()->route('posts.edit', $post) ;
+        return redirect()->route('posts.edit', $post);
     }
 
 
-    public function edit(Post $post){
-        return view('posts.edit', ['post'=>$post]);
-
-
+    public function edit(Post $post)
+    { // formulario de editar
+        return view('posts.edit', ['post' => $post]);
     }
+
+    public function update(Request $request, Post $post)
+    { // función de editar y guardar
+        $request->validate([
+            'title'=> 'required',
+            'slug'=> 'required|unique:posts,slug,' . $post->id, //unique:table,column,except,id',
+            'body'=> 'required',
+
+        ]);
+
+        $post->update([
+            'title' => $request->title,
+            'slug' => $request->slug,
+            'body' => $request->body,
+
+        ]);
+        return redirect()->route('posts.index', $post);
+    }
+
+
+
+
 
     public function destroy(Post $post)
     {
@@ -48,7 +80,4 @@ class PostController extends Controller
 
         return back();
     }
-
-
-
 }
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -1,10 +1,31 @@
 @csrf
+<!-- Campo titulo -->
+<label class="uppercase text-white-700 text-xs">Título</label><br>
+<span class="text-xs text-red-500">@error('title') {{$message}} @enderror</span>
+<input 
+type="text" 
+name="title"   
+placeholder="Nombre" 
+class="rounded border-gray-200 w-full mb-4 text-black" 
+value="{{old('title', $post->title)}}">
+<!-- Campo URl -->
+<label class="uppercase text-white-700 text-xs">Slug o Url</label><br>
+<span class="text-xs text-red-500">@error('slug') {{$message}} @enderror</span>
+<input 
+type="text" 
+name="slug"   
+placeholder="slug" 
+class="rounded border-gray-200 w-full mb-4 text-black" 
+value="{{old('slug', $post->slug)}}">
 
-<label class="uppercase text-white-700 text-xs">Título</label>
-<input type="text" name="title"   placeholder="Nombre" class="rounded border-gray-200 w-full mb-4 text-black" value="{{$post->title}}">
-
-<label class="uppercase text-white-700 text-xs">Contenido</label>
-<textarea name="body" rows="10" placeholder="Descripción"  class="rounded border-gray-200 w-full mb-4 text-black">{{ $post->body}}</textarea>
+<!-- Campo descripción  -->
+<label class="uppercase text-white-700 text-xs">Contenido</label> <br>
+<span class="text-xs text-red-500">@error('body') {{$message}} @enderror</span>
+<textarea 
+name="body" 
+rows="10" 
+placeholder="Descripción"  
+class="rounded border-gray-200 w-full mb-4 text-black">{{ old('body', $post->body)}}</textarea>
 
 <div class=" flex justify-between items-center">
     <a href="{{ route('posts.index')}}" class="rounded px-4 py-2">Atrás</a>
